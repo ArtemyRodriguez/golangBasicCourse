200dcac12b6249682da1a1ea8b0d7eaf3876aed3
Artemy Rodriguez <67998415+ArtemyRodriguez@users.noreply.github.com>
2025-07-29 19:02:38 +0300

    4.6. Возвращаемое значение

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 
 func main() {
 
-	const IMTPower = 2
 	var userHeight float64
 	var userWeight float64
 	fmt.Println(">>> Калькулятор Индекса Массы тела <<<")
@@ -15,7 +14,7 @@ func main() {
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите ваш вес: ")
 	fmt.Scan(&userWeight)
-	IMT := userWeight / math.Pow(userHeight/100, IMTPower)
+	IMT := calculateIMT(userHeight, userWeight)
 	outputResult(IMT)
 
 }
@@ -23,4 +22,10 @@ func main() {
 func outputResult(imt float64) {
 	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
 	fmt.Print(result)
+}
+
+func calculateIMT(userHeight float64, userWeight float64) float64 {
+	const IMTPower = 2
+	IMT := userWeight / math.Pow(userHeight/100, IMTPower)
+	return IMT
 }
