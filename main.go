0e5b341c4cdac2d284254805c4f2462d5a81cc8d
Artemy Rodriguez <67998415+ArtemyRodriguez@users.noreply.github.com>
2025-07-29 19:38:10 +0300

    4.7. Несколько значений возврата

--- main.go
+++ main.go
@@ -6,14 +6,8 @@ import (
 )
 
 func main() {
-
-	var userHeight float64
-	var userWeight float64
 	fmt.Println(">>> Калькулятор Индекса Массы тела <<<")
-	fmt.Print("Введите ваш рост в сантиметрах: ")
-	fmt.Scan(&userHeight)
-	fmt.Print("Введите ваш вес: ")
-	fmt.Scan(&userWeight)
+	userHeight, userWeight := getUserInput()
 	IMT := calculateIMT(userHeight, userWeight)
 	outputResult(IMT)
 
@@ -28,4 +22,14 @@ func calculateIMT(userHeight float64, userWeight float64) float64 {
 	const IMTPower = 2
 	IMT := userWeight / math.Pow(userHeight/100, IMTPower)
 	return IMT
+}
+
+func getUserInput() (float64, float64) {
+	var userHeight float64
+	var userWeight float64
+	fmt.Print("Введите ваш рост в сантиметрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите ваш вес: ")
+	fmt.Scan(&userWeight)
+	return userHeight, userWeight
 }
