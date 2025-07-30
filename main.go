b3e68f53087adaa7a89280c079626047e63538e5
Artemy Rodriguez <67998415+ArtemyRodriguez@users.noreply.github.com>
2025-07-30 17:08:19 +0300

    5.8. Повторение вводa

--- main.go
+++ main.go
@@ -8,36 +8,33 @@ import (
 const IMTPower = 2
 
 func main() {
-	//циклы
-	for i := 0; i < 10; i++ {
-		if i == 5 {
-			//break
-			continue
+	for {
+		fmt.Println(">>> Калькулятор Индекса Массы тела <<<")
+		userHeight, userWeight := getUserInput()
+		IMT := calculateIMT(userHeight, userWeight)
+		outputResult(IMT)
+		isRepeatCalculation := checkRepeatCalculation()
+		if !isRepeatCalculation {
+			break
 		}
-		fmt.Printf("%d\n", i)
 	}
-	fmt.Println(">>> Калькулятор Индекса Массы тела <<<")
-	userHeight, userWeight := getUserInput()
-	IMT := calculateIMT(userHeight, userWeight)
-	switch {
-	case IMT < 16:
-		fmt.Println("У вас сильный дефицит массы тела!")
-	case IMT < 18.5:
-		fmt.Println("У вас недостаток веса.")
-	case IMT < 25:
-		fmt.Println("У вас нормальный вес!")
-	case IMT < 30:
-		fmt.Println("У вас избыток массы тела!")
-	default:
-		fmt.Println("У вас ожирение!")
-	}
-	outputResult(IMT)
-
 }
 
 func outputResult(imt float64) {
 	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
 	fmt.Print(result)
+	switch {
+	case imt < 16:
+		fmt.Println("У вас сильный дефицит массы тела!")
+	case imt < 18.5:
+		fmt.Println("У вас недостаток веса.")
+	case imt < 25:
+		fmt.Println("У вас нормальный вес!")
+	case imt < 30:
+		fmt.Println("У вас избыток массы тела!")
+	default:
+		fmt.Println("У вас ожирение!")
+	}
 }
 
 func calculateIMT(userHeight float64, userWeight float64) float64 {
@@ -46,6 +43,7 @@ func calculateIMT(userHeight float64, userWeight float64) float64 {
 }
 
 func getUserInput() (float64, float64) {
+
 	var userHeight float64
 	var userWeight float64
 	fmt.Print("Введите ваш рост в сантиметрах: ")
@@ -53,4 +51,15 @@ func getUserInput() (float64, float64) {
 	fmt.Print("Введите ваш вес: ")
 	fmt.Scan(&userWeight)
 	return userHeight, userWeight
+
+}
+
+func checkRepeatCalculation() bool {
+	var userChoise string
+	fmt.Print("Вы хотите повторить расчет(y/n): ")
+	fmt.Scan(&userChoise)
+	if userChoise == "y" || userChoise == "Y" {
+		return true
+	}
+	return false
 }
